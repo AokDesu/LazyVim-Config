854c20a31f9a532de6bbe4948d40a10daf3c22fb
Aok <otakugamerza1@gmail.com>
2026-01-21 17:17:14 +0700

    update: smear-cursor config

--- lua/plugins/nvim-smear-cursor.lua
+++ lua/plugins/nvim-smear-cursor.lua
@@ -1,8 +1,22 @@
 return {
-  "sphamba/smear-cursor.nvim",
-  opts = {
-    stiffness = 0.5,
-    trailing_stiffness = 0.5,
-    matrix_pixel_threshold = 0.5,
+  {
+    "sphamba/smear-cursor.nvim",
+    event = "VeryLazy",
+    cond = vim.g.neovide == nil,
+    opts = {
+      stiffness = 0.5,
+      trailing_stiffness = 0.5,
+      matrix_pixel_threshold = 0.5,
+    },
+    specs = {
+      -- disable mini.animate cursor
+      {
+        "nvim-mini/mini.animate",
+        optional = true,
+        opts = {
+          cursor = { enable = false },
+        },
+      },
+    },
   },
 }
